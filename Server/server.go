1c9b47203056257a9655d64798d4f57ab3327079
Manuel <78505061+manidmt@users.noreply.github.com>
2024-08-29 15:16:01 +0100

    Server.go implementation for multiple clients

--- Server/server.go
+++ Server/server.go
@@ -1,3 +1,15 @@
+/**
+ * Server
+ *
+ * This is the server side of the attestation protocol.
+ * It receives the TPM data from the client, verifies it and generates a challenge.
+ * The client sends the challenge back to the server, which verifies it and generates a nonce.
+ * The client sends the nonce back to the server, which verifies the attestation data and registers the client in the database.
+ *
+ * @authors: Manuel Díaz-Meco, Miguel Ángel Mesa
+ * @version: 2.0
+ */
+
 package main
 
 import (
@@ -13,17 +25,26 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math/big"
 	"net/http"
 	"os"
 	"path"
 	"reflect"
 	"regexp"
+	"sync"
 	"time"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/google/go-attestation/attest"
 	"github.com/tjarratt/babble"
 )
+
+/**
+ * Message struct
+ *
+ * This struct is used to store the message received from the client.
+ * It contains the TPM version, the EK public key, the EK certificate and the attestation parameters.
+ */
 
 type Message struct {
 	TPMVersion attest.TPMVersion
@@ -32,26 +53,64 @@ type Message struct {
 	AK         attest.AttestationParameters
 }
 
+/**
+ * attestParams struct
+ *
+ * This struct is used to store the attestation parameters received from the client.
+ * It contains the platform parameters and the AK public key.
+ */
+
 type attestParams struct {
 	Parameters attest.PlatformParameters
 	AKPublic   []byte
 }
 
-var tpmInfoReceived = false
-var isSecretReceived = false
-var paramReceived = false
-var nonceGenerated = false
-var secret []byte
-var nonce []byte
-var m Message
-
-func start() {
-
-	tpmInfoReceived = false
-	isSecretReceived = false
-	paramReceived = false
-	nonceGenerated = false
+/**
+ * ClientData struct
+ *
+ * This struct is used to store the data of the client that is connecting to the server.
+ * It contains the secret, nonce and the message that the client sends to the server.
+ * The secret is used to verify the client's identity, the nonce is used to verify the attestation data
+ * and the message contains the data that the client sends to the server.
+ */
+type ClientData struct {
+	secret []byte
+	nonce  []byte
+	m      Message
 }
+
+var clients = make(map[string]ClientData)
+var mutex = &sync.Mutex{}
+
+/**
+ * generateRandomID function
+ *
+ * This function generates a random ID.
+ * It generates a random number and converts it to a string.
+ *
+ * @param max int64 - The maximum value of the random number.
+ *
+ * @return string - The random ID generated.
+ * @return error - The error that occurred while generating the random ID.
+ */
+
+func generateRandomID(max int64) (string, error) {
+	n, err := rand.Int(rand.Reader, big.NewInt(max))
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%d", n.Int64()+1), nil
+}
+
+/**
+ * generateNonce function
+ *
+ * This function generates a random nonce of 32 bytes.
+ * It uses the crypto/rand package to generate the random bytes.
+ *
+ * @return []byte - The nonce generated.
+ * @return error - The error that occurred while generating the nonce.
+ */
 
 func generateNonce() ([]byte, error) {
 
@@ -64,7 +123,19 @@ func generateNonce() ([]byte, error) {
 	return nonce, nil
 }
 
-// VerifyCertificate checks if the certificate is signed by a valid CA
+/**
+ * VerifyCertificate function
+ *
+ * This function verifies the certificate of the EK.
+ * It reads the CA certificate from the file system and verifies the EK certificate.
+ *
+ * @param caCertPath string - The path to the CA certificate.
+ * @param cert x509.Certificate - The certificate to verify.
+ *
+ * @return bool - The result of the verification.
+ * @return error - The error that occurred while verifying the certificate.
+ */
+
 func VerifyCertificate(caCertPath string, cert x509.Certificate) (bool, error) {
 
 	//First step is to removed unhandled extensions, otherwise Verify will fail
@@ -98,7 +169,18 @@ func VerifyCertificate(caCertPath string, cert x509.Certificate) (bool, error) {
 	return true, nil
 }
 
-// This function downloads the Intermediate CA that is referenced in the Certificate
+/**
+ * downloadCACert function
+ *
+ * This function downloads the CA certificate from the URL provided.
+ * It reads the certificate from the URL and saves it to the file system.
+ *
+ * @param URL string - The URL to download the CA certificate from.
+ *
+ * @return string - The name of the file where the certificate was saved.
+ * @return error - The error that occurred while downloading the certificate.
+ */
+
 func downloadCACert(URL string) (string, error) {
 
 	req, _ := http.NewRequest("GET", URL, nil)
@@ -133,6 +215,18 @@ func downloadCACert(URL string) (string, error) {
 	}
 	return CertName, nil
 }
+
+/**
+ * verifyTPMData function
+ *
+ * This function verifies the TPM data received from the client.
+ * It checks the URL inside the certificate and downloads the CA certificate.
+ * It then verifies the certificate using the CA certificate.
+ *
+ * @param m Message - The message received from the client.
+ *
+ * @return error - The error that occurred while verifying the TPM data.
+ */
 
 func verifyTPMData(m Message) error {
 
@@ -181,6 +275,19 @@ func verifyTPMData(m Message) error {
 
 	return nil
 }
+
+/**
+ * generateChallenge function
+ *
+ * This function generates a challenge for the client.
+ * It generates the activation parameters for the client and returns the challenge.
+ *
+ * @param m Message - The message received from the client.
+ *
+ * @return []byte - The challenge generated.
+ * @return []byte - The secret generated.
+ * @return error - The error that occurred while generating the challenge.
+ */
 
 func generateChallenge(m Message) ([]byte, []byte, error) {
 
@@ -241,6 +348,20 @@ func generateChallenge(m Message) ([]byte, []byte, error) {
 	return ec2s, secret, nil
 }
 
+/**
+ * verifyAttestationData function
+ *
+ * This function verifies the attestation data received from the client.
+ * It verifies the attestation data by checking the PCR values and the nonce.
+ *
+ * @param params attestParams - The attestation parameters received from the client.
+ * @param m Message - The message received from the client.
+ * @param nonce []byte - The nonce generated.
+ *
+ * @return attest.PCR - PRC value that will be used to register the Client in the Data Base.
+ * @return error - The error that occurred while verifying the attestation data.
+ */
+
 func verifyAttestationData(params attestParams, m Message, nonce []byte) (attest.PCR, error) {
 
 	// Check if the attestation data is correct by checking if there is a difference
@@ -275,8 +396,23 @@ func verifyAttestationData(params attestParams, m Message, nonce []byte) (attest
 		}
 	}
 
-	return parameters.PCRs[10], nil
+	return parameters.
+		PCRs[10], nil
 }
+
+/**
+ * registerClient function
+ *
+ * This function registers the client in the database.
+ * It stores the client's data in the database.
+ *
+ * @param db *sql.DB - The database connection.
+ * @param m Message - The message received from the client.
+ * @param boot_aggregate attest.PCR - The PCR value to store in the database.
+ *
+ * @return string - The session ID generated.
+ * @return error - The error that occurred while registering the client.
+ */
 
 func registerClient(db *sql.DB, m Message, boot_aggregate attest.PCR) (string, error) {
 
@@ -326,6 +462,19 @@ func registerClient(db *sql.DB, m Message, boot_aggregate attest.PCR) (string, e
 	return SessionID, nil
 }
 
+/**
+ * writeToClient function
+ *
+ * This function writes data to the client.
+ * It writes the data to the client's response.
+ *
+ * @param w http.ResponseWriter - The response writer.
+ * @param m2s []byte - The data to write to the client.
+ * @param content_t string - The content type of the data.
+ *
+ * @return error - The error that occurred while writing to the client.
+ */
+
 func writeToClient(w http.ResponseWriter, m2s []byte, content_t string) error {
 
 	w.Header().Set("Content-Type", content_t)
@@ -337,6 +486,18 @@ func writeToClient(w http.ResponseWriter, m2s []byte, content_t string) error {
 	return nil
 }
 
+/**
+ * readFromClient function
+ *
+ * This function reads data from the client.
+ * It reads the data from the client's request.
+ *
+ * @param r *http.Request - The request from the client.
+ *
+ * @return []byte - The data read from the client.
+ * @return error - The error that occurred while reading from the client.
+ */
+
 func readFromClient(r *http.Request) ([]byte, error) {
 
 	body, err := io.ReadAll(r.Body)
@@ -347,116 +508,211 @@ func readFromClient(r *http.Request) ([]byte, error) {
 	return body, nil
 }
 
-func handleConnection(w http.ResponseWriter, r *http.Request) {
+/**
+ * handleTPMData function
+ *
+ * This function handles the TPM data received from the client.
+ * It verifies the TPM data and generates a challenge for the client.
+ *
+ * @param w http.ResponseWriter - The response writer.
+ * @param r *http.Request - The request from the client.
+ */
 
-	if !tpmInfoReceived {
+func handleTPMData(w http.ResponseWriter, r *http.Request) {
 
-		defer r.Body.Close()
-		message, err := readFromClient(r)
-		if err != nil {
-			return
+	r.ParseForm()
+	cookie, err := r.Cookie("clientID")
+	if err != nil {
+		var clientID string
+		for {
+			clientID, err = generateRandomID(1000000)
+			if err != nil {
+				http.Error(w, "Error generating client ID", http.StatusInternalServerError)
+				return
+			}
+
+			// Verify that the client ID is not already in use
+			mutex.Lock()
+			_, exists := clients[clientID]
+			mutex.Unlock()
+
+			if !exists {
+				break
+			}
 		}
 
-		fmt.Printf("Message received: %v \n", message[:10])
-		json.Unmarshal(message, &m) //Unmarshalling the message
-
-		verifyTPMData(m)
-		var ec2s []byte
-		ec2s, secret, err = generateChallenge(m)
-		if err != nil {
-			return
+		cookie = &http.Cookie{
+			Name:  "clientID",
+			Value: clientID,
+			Path:  "/",
 		}
-		err = writeToClient(w, ec2s, "application/json")
-		if err != nil {
-			return
-		}
-		tpmInfoReceived = true
+		http.SetCookie(w, cookie)
 	}
 
-	if !isSecretReceived {
+	mutex.Lock()
+	defer mutex.Unlock()
 
-		defer r.Body.Close()
-		secretReceived, err := readFromClient(r)
-		if err != nil {
-			start()
-			return
-		}
-
-		if len(secretReceived) < 1 {
-			fmt.Print("No secret received, retrying...\n")
-			return
-		}
-
-		//fmt.Printf("Secret sent: %v \n", secret[:10])
-		//fmt.Printf("Secret received: %v \n", secretReceived[:10])
-
-		if reflect.DeepEqual(secret, secretReceived) {
-			fmt.Fprintf(os.Stdout, "Secrets match, challenge was successful! \n")
-		} else {
-			fmt.Fprintf(os.Stderr, "Secrets do not match, challenge failed! \n")
-			start()
-			return
-		}
-		isSecretReceived = true
+	defer r.Body.Close()
+	message, err := readFromClient(r)
+	if err != nil {
+		http.Error(w, "Error reading from client\n", http.StatusBadRequest)
+		return
 	}
 
-	if !nonceGenerated {
-		fmt.Print("Generating nonce... \n")
-		var err error
-		nonce, err = generateNonce()
-		if err != nil {
-			start()
-			return
-		}
-		err = writeToClient(w, nonce, "application/octet-stream")
-		if err != nil {
-			start()
-			return
-		}
-		nonceGenerated = true
+	var m Message
+
+	//fmt.Printf("Message received: %v \n", message[:10])
+	json.Unmarshal(message, &m) //Unmarshalling the message
+
+	verifyTPMData(m)
+	var ec2s []byte
+	ec2s, secret, err := generateChallenge(m)
+	if err != nil {
+		http.Error(w, "Error generating challenge\n", http.StatusBadRequest)
+		return
 	}
-
-	if !paramReceived {
-		defer r.Body.Close()
-		attestationData, err := readFromClient(r)
-		if err != nil || len(attestationData) < 1 {
-			fmt.Print("No attestation data received, retrying...\n")
-			return
-		}
-
-		//fmt.Printf("Param received: %v\n", attestationData)
-		var params attestParams
-		json.Unmarshal(attestationData, &params)
-		boot_aggregate, err := verifyAttestationData(params, m, nonce)
-		if err != nil {
-			start()
-			return
-		}
-
-		var db *sql.DB
-		sessionID, err := registerClient(db, m, boot_aggregate)
-		if err != nil {
-			start()
-			return
-		}
-		fmt.Printf("Client registered successfully! \n")
-
-		err = writeToClient(w, []byte(sessionID), "application/octet-stream")
-		if err != nil {
-			start()
-			return
-		}
-		paramReceived = true
-	}
-
-	if paramReceived && isSecretReceived && tpmInfoReceived && nonceGenerated {
-		start()
+	clients[cookie.Value] = ClientData{secret, nil, m}
+	err = writeToClient(w, ec2s, "application/json")
+	if err != nil {
+		http.Error(w, "Error writing to client\n", http.StatusBadRequest)
+		return
 	}
 }
 
+/**
+ * handleChallegne function
+ *
+ * This function handles the challenge received from the client.
+ * It verifies the challenge and generates a nonce for the client.
+ *
+ * @param w http.ResponseWriter - The response writer.
+ * @param r *http.Request - The request from the client.
+ */
+
+func handleChallenge(w http.ResponseWriter, r *http.Request) {
+
+	r.ParseForm()
+	cookie, err := r.Cookie("clientID")
+	if err != nil {
+		http.Error(w, "Client not identified", http.StatusBadRequest)
+		return
+	}
+
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	defer r.Body.Close()
+	secretReceived, err := readFromClient(r)
+	if err != nil {
+		http.Error(w, "Error reading from client\n", http.StatusBadRequest)
+		return
+	}
+
+	if len(secretReceived) < 1 {
+		http.Error(w, "No secret received\n", http.StatusBadRequest)
+		return
+	}
+
+	client := clients[cookie.Value]
+
+	//fmt.Printf("Secret sent: %v \n", secret[:10])
+	//fmt.Printf("Secret received: %v \n", secretReceived[:10])
+
+	if reflect.DeepEqual(client.secret, secretReceived) {
+		fmt.Printf("Secrets match, client with ID %s passed the challenge! \n", cookie.Value)
+	} else {
+		fmt.Printf("Secrets do not match, client with ID %s did not pass the challenge! \n", cookie.Value)
+		http.Error(w, "Secrets do not match, challenge failed! \n", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("Generating nonce for client with ID:%s... \n", cookie.Value)
+
+	nonce, err := generateNonce()
+	if err != nil {
+		http.Error(w, "Error generating nonce\n", http.StatusBadRequest)
+		return
+	}
+	client.nonce = nonce
+	//fmt.Printf("Nonce generated: %s ,%v\n", cookie.Value, client.nonce)
+	err = writeToClient(w, client.nonce, "application/octet-stream")
+	if err != nil {
+		http.Error(w, "Error writing to client\n", http.StatusBadRequest)
+		return
+	}
+	clients[cookie.Value] = client
+}
+
+/**
+ * handleDBRegister function
+ *
+ * This function handles the database registration received from the client.
+ * It verifies the attestation data and registers the client in the database.
+ *
+ * @param w http.ResponseWriter - The response writer.
+ * @param r *http.Request - The request from the client.
+ */
+
+func handleDBRegister(w http.ResponseWriter, r *http.Request) {
+
+	r.ParseForm()
+	cookie, err := r.Cookie("clientID")
+	if err != nil {
+		http.Error(w, "Cliente not identified", http.StatusBadRequest)
+		return
+	}
+
+	mutex.Lock()
+	client, ok := clients[cookie.Value]
+	mutex.Unlock()
+	if !ok {
+		http.Error(w, "Client Data not found", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+	attestationData, err := readFromClient(r)
+	if err != nil || len(attestationData) < 1 {
+		http.Error(w, "No attestation data received\n", http.StatusBadRequest)
+		return
+	}
+
+	//fmt.Printf("Param received: %v\n", attestationData)
+	var params attestParams
+	json.Unmarshal(attestationData, &params)
+	boot_aggregate, err := verifyAttestationData(params, client.m, client.nonce)
+	if err != nil {
+		http.Error(w, "Error verifying attestation data\n", http.StatusBadRequest)
+		return
+	}
+
+	var db *sql.DB
+	sessionID, err := registerClient(db, client.m, boot_aggregate)
+	if err != nil {
+		http.Error(w, "Error registering client\n", http.StatusBadRequest)
+		return
+	}
+	fmt.Printf("Client with ID:%s was registered successfully! \n", cookie.Value)
+
+	err = writeToClient(w, []byte(sessionID), "application/octet-stream")
+	if err != nil {
+		http.Error(w, "Error writing to client\n", http.StatusBadRequest)
+		return
+	}
+}
+
+/**
+ * main function
+ *
+ * This is the main function of the server.
+ * It starts the server and listens for incoming connections.
+ */
+
 func main() {
 
-	http.HandleFunc("/", handleConnection)
+	http.HandleFunc("/tpmData", handleTPMData)
+	http.HandleFunc("/challenge", handleChallenge)
+	http.HandleFunc("/dbRegistation", handleDBRegister)
 	fmt.Println("Serving on port: 8080")
 	err := http.ListenAndServe(":8080", nil)
 	if errors.Is(err, http.ErrServerClosed) {
